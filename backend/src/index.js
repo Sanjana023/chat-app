2c76799dd0a8fffe6e32e6f5d77414289ae1c450
Sanjana023 <duttasanjana237@gmail.com>
2025-06-29 22:17:03 +0530

    deployment crash fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,14 +26,13 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// ✅ Serve frontend in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
 
 server.listen(PORT, () => {
   console.log("Server is running on port:", PORT);
