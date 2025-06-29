67a5491a2d572e83ca90824d5a5953e391576df7
Sanjana023 <duttasanjana237@gmail.com>
2025-06-29 19:27:26 +0530

    realtime messaging

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -7,13 +7,18 @@ import { useAuthStore } from "../store/useAuthStore"
 import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
-  const {messages , getMessages , isMessagesLoading , selectedUser} =useChatStore()
+  const {messages , getMessages , isMessagesLoading , selectedUser,subscribeToMessages,
+    unsubscribeFromMessages} =useChatStore()
   const { authUser }=useAuthStore();
   const messageEndRef = useRef(null);
 
   useEffect( () => {
     getMessages(selectedUser._id)
-  },[selectedUser._id, getMessages])
+
+    subscribeToMessages();
+
+    return () =>unsubscribeFromMessages();
+  },[selectedUser._id, getMessages,subscribeToMessages , unsubscribeFromMessages])
 
   if(isMessagesLoading) return (
     <div className="flex-1 flex flex-col overflow-auto">
